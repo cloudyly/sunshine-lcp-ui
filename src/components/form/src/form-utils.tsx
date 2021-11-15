380b3f7d35ec488cf54640a39a41f3ad72216d67
liuyun <yunliuly@cn.ibm.com>
2021-11-15 23:30:53 +0800

    feat: form-添加number类型的解析

--- src/components/form/src/form-utils.tsx
+++ src/components/form/src/form-utils.tsx
@@ -20,65 +20,80 @@ export const renderFormItem = (
   if (uiItem[UI_HIDDEN]) {
     return null
   }
-  const { type, anyOf, oneOf, format } = item
+  const { type, oneOf, format } = item
   const commonProps = uiItem[UI_OPTIONS] || {}
   commonProps.disabled = (uiItem[UI_DISABLED] === true)
   const generateItem = () => {
-    if (type === PropItemTypes.STRING) {
-      if (!anyOf && !oneOf && !format) {
-        return (
-          <el-input
-            v-model={form[prop]}
-            {...commonProps}
-          />
-        )
-      }
-      if (format === 'date') {
-        return (
-          <el-date-picker
-            v-model={form[prop]}
-            type="date"
-            {...commonProps}
-          />
-        )
-      }
-      // if (format === 'time') {
-      //   return (
-      //     <el-time-picker
-      //       v-model={form[prop]}
-      //       {...commonProps}
-      //     />
-      //   )
-      // }
-      if (oneOf && oneOf.length > 0) {
-        const uiWidget = uiItem[UI_WIDGET] || 'select'
-        if (uiWidget === UiWidgets.SELECT) {
+    switch (type) {
+      case PropItemTypes.STRING: {
+        if (!oneOf && !format) {
           return (
-            <el-select v-model={form[prop]} {...commonProps}>
-              {
-                oneOf.map(one => (
-                  <el-option label={one.title} value={one.const}/>
-                ))
-              }
-            </el-select>
+            <el-input
+              v-model={form[prop]}
+              {...commonProps}
+            />
           )
-        } else if (uiWidget === UiWidgets.RADIO) {
-          return (
-            <el-radio-group v-model={form[prop]} {...commonProps}>
-              {
-                oneOf.map(one => (
-                  <el-radio label={one.const}>{one.title}</el-radio>
-                ))
-              }
-            </el-radio-group>
-          )
-        } else {
-          throw Error('oneOf 只能使用 radio 或 select')
         }
+        if (format === 'date') {
+          return (
+            <el-date-picker
+              v-model={form[prop]}
+              type="date"
+              {...commonProps}
+            />
+          )
+        }
+        // if (format === 'time') {
+        //   return (
+        //     <el-time-picker
+        //       v-model={form[prop]}
+        //       {...commonProps}
+        //     />
+        //   )
+        // }
+        if (oneOf && oneOf.length > 0) {
+          const uiWidget = uiItem[UI_WIDGET] || 'select'
+          if (uiWidget === UiWidgets.SELECT) {
+            return (
+              <el-select v-model={form[prop]} {...commonProps}>
+                {
+                  oneOf.map(one => (
+                    <el-option label={one.title} value={one.const}/>
+                  ))
+                }
+              </el-select>
+            )
+          } else if (uiWidget === UiWidgets.RADIO) {
+            return (
+              <el-radio-group v-model={form[prop]} {...commonProps}>
+                {
+                  oneOf.map(one => (
+                    <el-radio label={one.const}>{one.title}</el-radio>
+                  ))
+                }
+              </el-radio-group>
+            )
+          } else {
+            throw Error('oneOf 只能使用 radio 或 select')
+          }
+        }
+        break
       }
-      return <div>Other String</div>
+      case PropItemTypes.NUMBER: {
+        return (
+          <el-input-number
+            v-model={form[prop]}
+            {...commonProps}
+          />
+        )
+        break
+      }
+      // case PropItemTypes.BOOLEAN: {
+      //   break
+      // }
+      default:
+        return <div>暂不支持类型 {type}</div>
     }
-    return <div>Other Type</div>
   }
   const uiColumn = uiItem[UI_COLUMN] || 1
   return (
